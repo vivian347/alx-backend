8fda93d720f5f29042d55e714ff72ad18c5629cf
vivian347 <vkinyua001@gmail.com>
2023-04-23 14:35:28 +0300

    caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,9 +5,15 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """inherits from BaseCaching and is a caching system
+    """ LRU caching system
     Use of OrderedDict which keep order of insertion of keys
-    The order shows how recently they were used."""
+    The order shows how recently they were used.
+    In the beginning is the least recently used and in the end,
+    the most recently used.
+    Any update OR query made to a key moves to the end (most recently used).
+    If anything is added, it is added at the end (most recently used/added).
+    All operations have O(1) time complexity.
+    """
 
     def __init__(self):
         """initialize class"""
@@ -15,8 +21,13 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """use LRU algos to assign dict valsss
-        Add an item in the cache"""
+        """ Add an item in the cache
+        First, add/ update the key by conventional methods.
+        And also move the key to the end to show that it was recently used.
+        But here we will also check if the length of our dictionary
+        has exceeded our capacity.
+        If so remove the first key (least recently used)
+        """
         if key and item:
             self.cache_data[key] = item
             self.cache_data.move_to_end(key)
@@ -24,11 +35,14 @@ class LRUCache(BaseCaching):
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             discarded = self.cache_data.popitem(last=False)
 
-            print("DISCARD: {}".format(discarded[0]))
+            print('DISCARD: {}'.format(discarded[0]))
 
     def get(self, key):
         """ Get an item by key
-        return val in dict linked to key"""
+        Return the value of the key that is queried in O(1)
+        and return -1 if the key is not found.
+        And also move the key to the end to show that it was recently used
+        """
         if key in self.cache_data and key is not None:
             self.cache_data.move_to_end(key)
             return self.cache_data[key]
