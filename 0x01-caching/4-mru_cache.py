f907de26dfe8f070e5591b98cdeee0f26c38b454
vivian347 <vkinyua001@gmail.com>
2023-04-23 14:28:34 +0300

    caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,43 +1,41 @@
-#!/usr/bin/env python3
-"""MRU cache"""
-
+#!/usr/bin/python3
+""" Python caching systems """
+from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-BaseCaching = __import__('base_caching').BaseCaching
-
-
 class MRUCache(BaseCaching):
-    """inherits from BaseCaching and is a caching system"""
+    """ MRU caching system
+    """
 
     def __init__(self):
-        """initialize class"""
+        """ Initialize class instance. """
         super().__init__()
         self.cache_data = OrderedDict()
         self.mru = ""
 
     def put(self, key, item):
-        """use MRU algos to assign dict valsss"""
+        """ Add an item in the cache
+        """
         if key and item:
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 if key in self.cache_data:
                     self.cache_data.update({key: item})
                     self.mru = key
                 else:
+                    # discard the most recently used item
                     discarded = self.mru
                     del self.cache_data[discarded]
                     print("DISCARD: {}".format(discarded))
                     self.cache_data[key] = item
                     self.mru = key
-
             else:
                 self.cache_data[key] = item
                 self.mru = key
 
     def get(self, key):
-        """return val in dict linked to key"""
-        if key in self.cache_data and key is not None:
+        """ Get an item by key
+        """
+        if key in self.cache_data:
             self.mru = key
             return self.cache_data[key]
-        else:
-            return None
