39896d1edf084217122a713c1825d8b2e18f2a3b
vivian347 <vkinyua001@gmail.com>
2023-04-23 14:48:33 +0300

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,22 +10,18 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """initialize"""
         super().__init__()
-        self.key_list = []
 
     def put(self, key, item):
         """use FIFO algos to assign dict valsss"""
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            oldest_key = self.key_list.pop(0)
+        if key and item:
+            self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest_key = sorted(self.cache_data)[0]
             del self.cache_data[oldest_key]
             print("DISCARD: ", oldest_key)
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
+
         self.key_list.append(key)
 
     def get(self, key):
         """return val in dict linked to key"""
-        if key in self.cache_data and key is not None:
-            return self.cache_data[key]
-        else:
-            return None
+        return self.cache_data[key]
